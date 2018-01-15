68ae889470212213454215c55e3c161357e638ca
Dave Schlauderaff <dschlaud@gmail.com>
2018-01-14 21:54:53 -0600

    phase 1 of tutorial, build form with ajax, lots of trial and error to get jquery running with webpack

---
+++ frontend/components/appointments.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Appointments = () => (
+  <h1>CalReact</h1>
+)
+
+export default Appointments
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,12 +1,18 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import React from "react";
+import ReactDOM from "react-dom";
+import Rails from "rails-ujs";
+import jQuery from "jquery";
+import "init";
+import Appointment from "../components/appointments.jsx";
 
-import "./application.css";
+Rails.start();
+window.$ = window.jQuery;
+window.jQuery = jQuery;
 
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(
+//     <Appointment />,
+//     document.body.appendChild(document.createElement('div')
+//     ),
+//   )
+// })
